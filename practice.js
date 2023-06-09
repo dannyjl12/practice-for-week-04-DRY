d7d61e720419a7fd9cc14d1f14d9ed5d0ee5737d
danny-lowton <dannylowton@gmail.com>
2023-06-09 12:46:54 -0400

    completed

--- practice.js
+++ practice.js
@@ -1,46 +1,57 @@
-function multiplyBiggerNumByTwo(num1, num2) {
+const findBig = (num1, num2) => {
   let bigNum;
   if (num1 > num2) {
     bigNum = num1;
-    return bigNum * 2;
   } else {
     bigNum = num2;
-    return bigNum * 2;
   }
+  return bigNum;
 }
+
+function multiplyBiggerNumByTwo(num1, num2) {
+  let bigger = findBig(num1, num2);
+  return bigger * 2;
+}
+
+// find biggest number
+// multiply it by 2
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  let bigger = findBig(num1, num2);
+  return bigger / 3;
 }
 
+// find biggest number
+// divide it by 3
+
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+
+  let bigNum = findBig(sum1, sum2);
+  return `I ate ${bigNum} tacos.`;
 }
+
+// find biggest number
+// add it into the sentence
 
 function adoptSmallerDog(weight1, weight2) {
   let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
+  if (weight1 === findBig(weight1, weight2)) {
     smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
+  } else {
+    smallDog = weight1;
   }
+return `I adopted a dog that weighs ${smallDog} pounds.`;
 }
+
+// find smallest number
+// add it into the sentence
+
+// create a helper function
+// function that takes in 2 numbers and finds the biggest
+
+
+/***************************************          HELPER FUNCTION          ***************************************/
+
 
 
 /**************************************************************************/
